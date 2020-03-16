af9694afb66d37299f355b1c9f390ce004e17ec5
blayda <zacblayda@gmail.com>
2020-03-15 21:40:53 -0700

    adding images

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # friend-finder
+
+file:///Users/zacblayda/Desktop/Screen%20Shot%202020-03-15%20at%209.36.49%20PM.png
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,67 @@
 var friends = [
   {
-    name: "Test 1",
-    photos: "Test",
-    scores: ["5", "1", "5", "1", "4", "2", "4", "1", "3", "1"]
+    "name": "Test 1",
+    "photos": "Test",
+    "scores": [
+      "5",
+      "1",
+      "5",
+      "1",
+      "4",
+      "2",
+      "4",
+      "1",
+      "3",
+      "1"
+    ]
   },
   {
-    name: "Test 2",
-    photos: "Test",
-    scores: ["1", "4", "5", "2", "3", "4", "5", "4", "2", "1"]
+    "name": "Test 2",
+    "photos": "Test",
+    "scores": [
+      "1",
+      "4",
+      "5",
+      "2",
+      "3",
+      "4",
+      "5",
+      "4",
+      "2",
+      "1"
+    ]
   },
   {
-    name: "Test 3",
-    photos: "Test",
-    scores: ["3", "3", "1", "1", "5", "1", "3", "5", "1", "1"]
+    "name": "Test 3",
+    "photos": "Test",
+    "scores": [
+      "3",
+      "3",
+      "1",
+      "1",
+      "5",
+      "1",
+      "3",
+      "5",
+      "1",
+      "1"
+    ]
+  },
+  {
+    "name": "zac",
+    "photos": "adafadf",
+    "scores": [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
   }
-];
-module.exports = friends;
+]
+ module.exports = friends
